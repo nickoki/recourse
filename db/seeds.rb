08c7c74aaa4fb1b053332321382abfcaf9378fdd
Nick Oki <nick.oki@live.com>
2016-10-28 14:35:29 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Require seed data files
+require_relative './user_data.rb'
+require_relative './post_data.rb'
+
+# Drop tables
+User.destroy_all
+Post.destroy_all
+
+user_data = get_user_data()
+post_data = get_post_data()
+
+user_data.each_pair do |user_name, user_info|
+  new_user = User.create! user_info
+  new_post = post_data[user_name]
+  Post.create!(new_post.merge(user: new_user))
+end
---
+++ db/user_data.rb
@@ -0,0 +1,16 @@
+def get_user_data
+  user_data = {
+    "nickoki" => {
+      email: "nick@nick.nick",
+      password: "okioki"
+    },
+    "barrettcope" => {
+      email: "bar@bar.bar",
+      password: "barbar"
+    },
+    "timfoley" => {
+      email: "tim@tim.tim",
+      password: "timtim"
+    }
+  }
+end
