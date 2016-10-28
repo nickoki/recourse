c97ecb7db970a3bd502acaa143e73f6ec8801dd8
Nick Oki <nick.oki@live.com>
2016-10-28 14:20:13 -0400

    Fix Post migration, link and title not null

--- db/migrate/20161028181139_create_posts.rb
+++ db/migrate/20161028181139_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-      t.text :link
-      t.string :title
+      t.text :link, null: false
+      t.string :title, null: false
       t.string :level
       t.text :desc_what
       t.text :desc_why
