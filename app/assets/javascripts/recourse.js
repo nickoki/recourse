94d104a76105beee1dc7531d079400438a3b52cc
Nick Oki <nick.oki@live.com>
2016-10-31 19:27:04 -0400

    Refactor and comment recourse.js

--- app/assets/javascripts/recourse.js
+++ app/assets/javascripts/recourse.js
@@ -1,41 +1,62 @@
+// assets/javascrips/recourse.js
+
+// Initiate angular app
 angular
   .module("recourse", [
     "ui.router",
     "templates",
     "ngResource"
   ])
+
+  // Congifure router function
   .config([
     "$stateProvider",
     RouterFunction
   ])
+
+  // Declare factory for Post module
   .factory("PostFactory", [
     "$resource",
     PostFactoryFunction
   ])
+
+  // Declare controller for Post index
   .controller("PostIndexController", [
     "PostFactory",
     PostIndexControllerFunction
   ])
+
+  // Declare controller for Post show
   .controller("PostShowController", [
     "PostFactory",
     "$stateParams",
     PostShowControllerFunction
   ])
 
+
+
+// Post Factory Function
 function PostFactoryFunction($resource) {
   return $resource("/api/posts/:id.json", {}, {
     update: { method: "PUT" }
   })
 }
 
+
+
+// Index Post Controller Function
 function PostIndexControllerFunction(PostFactory) {
   this.posts = PostFactory.query()
 }
 
+// Show Post Controller Function
 function PostShowControllerFunction(PostFactory,$stateParams) {
   this.post = PostFactory.get({ id: $stateParams.id })
 }
 
+
+
+// Router Function
 function RouterFunction($stateParams) {
   $stateParams
     .state("postIndex", {
