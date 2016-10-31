957b517d1f7d140bbc15c02d5fe51c7042182b0e
Nick Oki <nick.oki@live.com>
2016-10-30 21:35:45 -0400

    Update auth.rb

--- lib/auth.rb
+++ lib/auth.rb
@@ -2,3 +2,23 @@
 
 # Use jwt gem
 require 'jwt'
+
+# Custom Authorization class
+class Auth
+
+  # Method to issue tokens
+  def self.issue(payload)
+    # Use encode method (from jwt gem) to generate a token
+    JWT.encode(payload, auth_secret, ALGORITHM)
+  end
+
+  # Method to decode tokens
+  def self.decode(token)
+    # Use decode method (from jwt gem) to interpret a token
+    JWT.decode(token, auth_secret, true, { algorithm: ALGORITHM }).first
+  end
+
+  def self.auth_secret
+    ENV["AUTH_SECRET"]
+  end
+end
